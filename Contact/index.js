0d34a1c6de14cd75ba2f7fe0c306d454b8db1485
Glen <Glen@GLEN>
2020-11-27 12:34:47 +0700

    update socket io thong bao loi moi ket ban

--- Contact/index.js
+++ Contact/index.js
@@ -3,13 +3,28 @@ const cors = require("cors");
 const app = express();
 const listen_port = 5000;
 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 app.use(cors());
 app.use(express.json());
 
 const userRouter = require("./api/router.js");
 app.use(userRouter);
 
+io.on('connection', (socket)=>{
+    console.log(socket.id + ": đã kết nối")
 
-app.listen(listen_port, () => {
+    socket.on('DangKyNhanThongBaoDanhBa', (data)=>{
+        socket.join(data.SoDienThoai)
+        socket.SoDienThoai = data.SoDienThoai
+    })
+
+    socket.on('DaGuiLoiMoiKetBan', (data)=>{
+        socket.broadcast.to(data.NguoiNhanLoiMoi).emit("ThongBaoLoiMoiKetBanMoi", data)
+    })
+})
+
+server.listen(listen_port, () => {
     console.log("Listening port: " + listen_port);
 });
