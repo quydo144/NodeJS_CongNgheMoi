64106e3fa0f4fc357db848511931e85e13da81dd
quydo144 <quydo144@gmail.com>
2020-11-28 12:45:28 +0700

    fix api create room

--- Chat/api/controller.js
+++ Chat/api/controller.js
@@ -4,13 +4,7 @@ module.exports = {
 
     addNameRoom(req, res, next) {
         const room = req.body;
-        services.findIDRoomByIdUser(room.id_1, room.id_2).then(data => {
-            if (data) {
-                res.json({ success: 0, message: "Tồn tại mã room", id_room: data.id_room })
-            }
-            else
-                return services.putNameRoom(room);
-        }).then(data => {
+        services.putNameRoom(room).then(data => {
             if (data)
                 res.json({ success: 1, message: "Thêm room thành công" });
         }).catch(err => {
@@ -20,26 +14,38 @@ module.exports = {
 
     deleteNameRoom(req, res, next) {
         const room = req.body;
-        services.findIDRoomByIdUser(room.id_1, room.id_2).then(data => {
-            if (!data)
-                res.json({ success: 0, message: "Không tồn tại ID room" });
-            else
-                return services.deleteRoomByID(data.id_room);
-        }).then(data => {
+        (   services.findIDRoomByIdUser12(room.id_user_1, room.id_user_2).then(data => {
+                if (data) {
+                    return services.deleteRoomByID(data.id_room);
+                }
+            }),
+            services.findIDRoomByIdUser21(room.id_user_2, room.id_user_1).then(data => {
+                if (data) {
+                    return services.deleteRoomByID(data.id_room);
+                }   
+            })
+        ).then(data => {
             res.json({ success: 1, message: "Xóa room thành công" });
         }).catch(err => {
             next(err);
         });
     },
 
-    findIDRoomByIdUser(req, res, next){
+    findIDRoomByIdUser(req, res, next) {
         const room = req.body;
-        services.findIDRoomByIdUser(room.id_1, room.id_2).then(data =>{
-            if(data){
+        services.findIDRoomByIdUser12(room.id_user_1, room.id_user_2).then(data => {
+            if (data) {
                 res.json({ success: 1, message: "Tồn tại mã room", id_room: data.id_room })
             }
-            else
-                res.json({ success: 0, message: "Không tồn tại room" })
+            else {
+                services.findIDRoomByIdUser21(room.id_user_2, room.id_user_1).then(data => {
+                    if (data) {
+                        res.json({ success: 1, message: "Tồn tại mã room", id_room: data.id_room })
+                    }
+                    else
+                        res.json({ success: 0, message: "Không tồn tại room" })
+                })
+            }
         })
     }
 }
--- Chat/api/service.js
+++ Chat/api/service.js
@@ -10,18 +10,42 @@ aws.config.update({
 const tableName = "ConversationName"
 const docClient = new aws.DynamoDB.DocumentClient();
 
-function findIDRoomByIdUser(id_1, id_2) {
+function findIDRoomByIdUser12(id_user_1, id_user_2) {
     return new Promise(function (resolve, reject) {
         docClient.scan({
             TableName: tableName,
-            FilterExpression: "#id in (:id_1, :id_2)",
+            FilterExpression: "#id_user_1=:id_user_1 and #id_user_2=:id_user_2",
             ExpressionAttributeNames: {
-                "#id": "id_1",
-                "#id": "id_2"
+                "#id_user_1": "id_user_1",
+                "#id_user_2": "id_user_2"
             },
             ExpressionAttributeValues: {
-                ":id_1": id_1,
-                ":id_2": id_2,
+                ":id_user_1": id_user_1,
+                ":id_user_2": id_user_2,
+            }
+        }, function (err, data) {
+            if (err){
+                reject(err)
+            }
+            else {
+                resolve(data.Items[0])
+            }
+        });
+    });
+}
+
+function findIDRoomByIdUser21(id_user_2, id_user_1) {
+    return new Promise(function (resolve, reject) {
+        docClient.scan({
+            TableName: tableName,
+            FilterExpression: "#id_user_1=:id_user_1 and #id_user_2=:id_user_2",
+            ExpressionAttributeNames: {
+                "#id_user_1": "id_user_1",
+                "#id_user_2": "id_user_2"
+            },
+            ExpressionAttributeValues: {
+                ":id_user_1": id_user_2,
+                ":id_user_2": id_user_1,
             }
         }, function (err, data) {
             if (err){
@@ -68,4 +92,4 @@ function deleteRoomByID(id) {
     });
 }
 
-module.exports = { findIDRoomByIdUser, putNameRoom, deleteRoomByID }
+module.exports = { findIDRoomByIdUser12, findIDRoomByIdUser21, putNameRoom, deleteRoomByID }
